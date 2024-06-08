37329de6e5075d27281376f2e345d36248d42f4e
MunsMan <munsman.github@gmail.com>
2024-06-08 12:36:54 +0200

    updating flake to build package

--- flake.nix
+++ flake.nix
@@ -15,11 +15,32 @@
         darwinInputs = with pkgs;
           lib.optionals stdenv.isDarwin
             (with pkgs.darwin.apple_sdk.framework; [ ]);
+
       in
       with pkgs; {
-        devShells.default = mkShell {
-          packages = [ rust rust-analyzer-unwrapped ] ++ darwinInputs;
+        devShells.default = mkShell
+          {
+            packages = [ rust rust-analyzer-unwrapped ] ++ darwinInputs;
+            shellHook = ''
+              export COLOR_SCHEMES_DIR=~/.config/color/
+              export COLOR_SCHEME=onedark
+            '';
+          };
+        packages = rec {
+          color_parser = rustPlatform.buildRustPackage {
+            pname = "color_parser";
+            version = "0.1.0";
+            src = ./color_parser;
+            cargoSha256 = "sha256-B/khcqX1COj56ch284g805qZsjDP1+d/uFA6gtxta48=";
+          };
+          default = pkgs.symlinkJoin
+            {
+              name = "sketchybar_utils";
+              paths = [ color_parser ];
+            };
         };
       });
 }
+
+
 
